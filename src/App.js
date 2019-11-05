324ce7563d405d1053fc20b9b8052f942c1ae671
Joshwa Moellenkamp <joshwa.moellenkamp@gmail.com>
2019-11-05 11:25:59 -0700

    Input fields are now contained inside of a material-ui table for a cleaner display.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
+import Table from '@material-ui/core/Table';
+import TableRow from '@material-ui/core/TableRow';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.handleContributionChange = this.handleContributionChange.bind(this);
-    this.state = { contribution: 0.0, interestRate: 7.0, years: 10, balance: 0.0 };
+    this.state = {
+      contribution: 0.0,
+      interestRate: 7.0,
+      years: 10,
+      balance: 0.0
+    };
   }
 
   handleContributionChange = event => {
@@ -23,6 +33,12 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <h3>401k Calculator</h3>
+        <p>
+          Configure the yearly contribution for employee and employer, predicted
+          interest return, number of years, and compounding method to predict
+          financial independence.
+        </p>
         <Calculator
           handleContributionChange={this.handleContributionChange}
           handleInterestRateChange={this.handleInterestRateChange}
@@ -36,11 +52,11 @@ class App extends React.Component {
           years={this.state.years}
           balance={this.state.balance}
         />
-        <Display
+        {/* <Display
           contribution={this.state.contribution}
           interestRate={this.state.interestRate}
           years={this.state.years}
-        />
+        /> */}
       </div>
     );
   }
@@ -49,18 +65,56 @@ class App extends React.Component {
 function Calculate(props) {
   let i;
   let balance = props.balance;
-  for(i = 1; i <= props.years; i++) {
-    const interest = balance * (.01 * props.interestRate);
+  for (i = 1; i <= props.years; i++) {
+    const interest = balance * (0.01 * props.interestRate);
     balance = balance + interest + props.contribution;
   }
 
-  return(
-    <h1>Balance: {StyleCurrency(balance)}</h1>
+  return (
+    <div>
+      <h3>Balance: {StyleCurrency(balance)}</h3>
+
+      {/* <Table style={{ tableLayout: "auto" }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Year</TableCell>
+            <TableCell>Total Contribution</TableCell>
+            <TableCell>Total Interest Earned</TableCell>
+            <TableCell>Total Balance</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {items.map((item) => (
+            <TableRow key={item}>
+              <TableCell>
+                <GreenButton variant="contained" onClick={(e) => challengeCompleted(item)}>
+                  Complete
+                </GreenButton>
+              </TableCell>
+              <TableCell>
+                {item}
+              </TableCell>
+              <TableCell>
+                {challenges.get(item).points}
+              </TableCell>
+              <TableCell>
+                {challenges.get(item).description}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table> */}
+    </div>
   );
 }
 
 function StyleCurrency(value) {
-  return value.toLocaleString(undefined, {style: "currency", currency: "USD", minimumFractionDigits: 2, maximumFractionDigits:2})
+  return value.toLocaleString(undefined, {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  });
 }
 
 function Display(props) {
@@ -76,32 +130,52 @@ function Display(props) {
 function Calculator(props) {
   return (
     <div>
-      <label>
-        Contribution (Yearly)
-        <input
-          type="number"
-          onChange={props.handleContributionChange}
-          value={props.contribution}
-        />
-      </label>
-      <p />
-      <label>
-        Interest Percentage (Compounded Yearly)
-        <input
-          type="number"
-          onChange={props.handleInterestRateChange}
-          value={props.interestRate}
-        />
-      </label>
-      <p />
-      <label>
-        Years
-        <input
-          type="number"
-          onChange={props.handleYearsChange}
-          value={props.years}
-        />
-      </label>
+      <Table style={{ tableLayout: "auto" }}>
+        <TableHead>
+          <TableRow>
+            <TableCell style={{ width: "50%", border: "none" }}></TableCell>
+            <TableCell style={{ width: "50%", border: "none"}}></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell align="right" style={{ border: "none" }}>
+              <label>Contribution (Yearly)</label>
+            </TableCell>
+            <TableCell style={{ border: "none" }}>
+              <input
+                type="number"
+                onChange={props.handleContributionChange}
+                value={props.contribution}
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="right" style={{ border: "none" }}>
+              <label>Interest Percentage</label>
+            </TableCell>
+            <TableCell style={{ border: "none" }}>
+              <input
+                type="number"
+                onChange={props.handleInterestRateChange}
+                value={props.interestRate}
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="right" style={{ border: "none" }}>
+              <label>Years</label>
+            </TableCell>
+            <TableCell style={{ border: "none" }}>
+              <input
+                type="number"
+                onChange={props.handleYearsChange}
+                value={props.years}
+              />
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 }
